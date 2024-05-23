27f1bbc2e56f7f81be8389017eaea277dfb83b45
kiv1n <aloha@kiv1n.ru>
2024-05-23 15:22:10 +0300

    Implement offer token contract

---
+++ contracts/contracts/OfferToken.sol
@@ -0,0 +1,125 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
+
+/**
+ * @notice A contract that stores offer tokens.
+ */
+contract OfferToken is ERC721URIStorage, FunctionsClient {
+    struct Content {
+        address recipient;
+        uint paymentAmount;
+        address paymentToken;
+        uint createdDate;
+        uint acceptDate;
+        string completeDataURI;
+        uint completeDate;
+        uint confirmDate;
+        bool confirmed;
+    }
+
+    uint private _nextTokenId;
+    mapping(uint tokenId => Content content) private _contents;
+    bytes32 private _functionsDonId;
+    address private _functionsRouter;
+
+    constructor(
+        bytes32 functionsDonId,
+        address functionsRouter
+    ) ERC721("Offer Token", "OFRT") FunctionsClient(functionsRouter) {
+        _functionsDonId = functionsDonId;
+        _functionsRouter = functionsRouter;
+    }
+
+    function create(
+        address recipient,
+        uint paymentAmount,
+        address paymentToken,
+        string memory uri
+    ) public {
+        uint256 tokenId = _nextTokenId++;
+        _mint(msg.sender, tokenId);
+        _setTokenURI(tokenId, uri);
+        Content memory content;
+        content.recipient = recipient;
+        content.paymentAmount = paymentAmount;
+        content.paymentToken = paymentToken;
+        content.createdDate = block.timestamp;
+        _contents[tokenId] = content;
+    }
+
+    // TODO: Check deadline
+    function accept(uint tokenId) public {
+        // Checks
+        require(_contents[tokenId].recipient == msg.sender, "Not recipient");
+        require(_contents[tokenId].acceptDate == 0, "Already accepted");
+        // Send tokens to contract
+        require(
+            IERC20(_contents[tokenId].paymentToken).transferFrom(
+                _ownerOf(tokenId),
+                address(this),
+                _contents[tokenId].paymentAmount
+            ),
+            "Failed to transfer payment to contract"
+        );
+        // Update content
+        _contents[tokenId].acceptDate = block.timestamp;
+    }
+
+    // TODO: Check deadline
+    function complete(uint tokenId, string memory completeDataURI) public {
+        // Checks
+        require(_contents[tokenId].recipient == msg.sender, "Not recipient");
+        require(_contents[tokenId].acceptDate != 0, "Not accepted");
+        require(_contents[tokenId].completeDate == 0, "Already completed");
+        // Update content
+        _contents[tokenId].completeDataURI = completeDataURI;
+        _contents[tokenId].completeDate = block.timestamp;
+    }
+
+    function confirm(uint tokenId) public {
+        // Checks
+        require(_contents[tokenId].recipient == msg.sender, "Not recipient");
+        require(_contents[tokenId].completeDate != 0, "Not completed");
+        require(_contents[tokenId].confirmDate == 0, "Already confirmed");
+        // Confirm without using functions
+        if (_functionsDonId == 0x0 || _functionsRouter == address(0)) {
+            _contents[tokenId].confirmDate = block.timestamp;
+            _contents[tokenId].confirmed = true;
+            require(
+                IERC20(_contents[tokenId].paymentToken).transfer(
+                    _contents[tokenId].recipient,
+                    _contents[tokenId].paymentAmount
+                ),
+                "Failed to transfer payment to recipient"
+            );
+        }
+        // Confirm with using functions
+        else {
+            // TODO: Send functions request
+        }
+    }
+
+    // TODO: Implement function for automation
+    function confirmBatch() public {}
+
+    function getNextTokenId() public view returns (uint nextTokenId) {
+        return _nextTokenId;
+    }
+
+    function getContent(
+        uint tokenId
+    ) public view returns (Content memory content) {
+        return _contents[tokenId];
+    }
+
+    // TODO: Get functions response and update tokens
+    function fulfillRequest(
+        bytes32 requestId,
+        bytes memory response,
+        bytes memory err
+    ) internal override {}
+}
---
+++ contracts/test/OfferToken.ts
@@ -0,0 +1,73 @@
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("OfferToken", function () {
+  async function initFixture() {
+    // Get signers
+    const [deployer, userOne, userTwo, userThree] = await ethers.getSigners();
+    // Deploy contracts
+    const usdTokenContractFactory = await ethers.getContractFactory("USDToken");
+    const usdTokenContract = await usdTokenContractFactory.deploy();
+    const offerTokenContractFactory = await ethers.getContractFactory(
+      "OfferToken"
+    );
+    const offerTokenContract = await offerTokenContractFactory.deploy(
+      ethers.ZeroHash,
+      ethers.ZeroAddress
+    );
+    // Send usd tokens to users
+    await usdTokenContract
+      .connect(deployer)
+      .transfer(userOne, ethers.parseEther("1000"));
+    await usdTokenContract
+      .connect(deployer)
+      .transfer(userTwo, ethers.parseEther("1000"));
+    return {
+      deployer,
+      userOne,
+      userTwo,
+      userThree,
+      usdTokenContract,
+      offerTokenContract,
+    };
+  }
+
+  // TODO: Delete only attribute
+  it.only("Should support the main flow", async function () {
+    const { userOne, userTwo, usdTokenContract, offerTokenContract } =
+      await loadFixture(initFixture);
+    // Create offer
+    await expect(
+      usdTokenContract
+        .connect(userOne)
+        .approve(offerTokenContract.getAddress(), ethers.MaxUint256)
+    ).to.be.not.reverted;
+    await expect(
+      offerTokenContract
+        .connect(userOne)
+        .create(userTwo, ethers.parseEther("42"), usdTokenContract, "ipfs://1")
+    ).to.be.not.reverted;
+    const tokenId = (await offerTokenContract.getNextTokenId()) - 1n;
+    // Accept offer
+    await expect(
+      offerTokenContract.connect(userTwo).accept(tokenId)
+    ).to.changeTokenBalances(
+      usdTokenContract,
+      [userOne, offerTokenContract],
+      [ethers.parseEther("-42"), ethers.parseEther("42")]
+    );
+    // Complete offer
+    await expect(
+      offerTokenContract.connect(userTwo).complete(tokenId, "ipfs://2")
+    ).to.be.not.reverted;
+    // Confirm offer
+    await expect(
+      offerTokenContract.connect(userTwo).confirm(tokenId)
+    ).to.changeTokenBalances(
+      usdTokenContract,
+      [userTwo, offerTokenContract],
+      [ethers.parseEther("42"), ethers.parseEther("-42")]
+    );
+  });
+});
