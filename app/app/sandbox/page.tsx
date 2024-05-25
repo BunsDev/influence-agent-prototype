3d0841183fab74eefca019bbb06817793620c617
kiv1n <aloha@kiv1n.ru>
2024-05-25 11:23:19 +0300

    Add empty sandbox page

---
+++ app/app/sandbox/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Separator } from "@/components/ui/separator";
+
+export default function SandboxPage() {
+  return (
+    <div className="container py-10 lg:px-80">
+      <div className="space-y-0.5">
+        <h2 className="text-2xl font-bold tracking-tight">Sandbox</h2>
+      </div>
+      <Separator className="my-6" />
+      {/* TODO: Implement */}
+    </div>
+  );
+}
